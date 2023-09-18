50dec2171ef5b3023120e5bdeb6be78661e669a4
arberlisaj <arberlisaj1@gmail.com>
2023-09-18 17:49:50 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import useFetchUser from "./hooks/useFetchUser";
+import Section from "./components/Section";
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div>App</div>;
+  const [inputValue, setInputValue] = useState<string>();
+  const { user, error } = useFetchUser(inputValue);
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
+  return (
+    <main className="bg-slate-100 min-h-screen">
+      <Header setInput={(value) => setInputValue(value)} />
+      {error && (
+        <>
+          <p className="text-3xl text-red-600 text-center mt-[20vh] mb-2">
+            Oops! Something went wrong
+          </p>
+          <Button
+            onClick={() => window.location.reload()}
+            variant="outline"
+            className="mx-auto block"
+          >
+            try again
+          </Button>
+        </>
+      )}
+
+      {!error && (
+        <Section>
+          <h1>{user?.login}</h1>
+        </Section>
+      )}
+    </main>
+  );
 };
 
 export default App;
